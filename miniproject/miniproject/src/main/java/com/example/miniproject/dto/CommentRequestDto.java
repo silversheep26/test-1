0e8a816ff285d809009dbd0b37f59991083d88ce
JH4 <125551746+spainclub@users.noreply.github.com>
2023-05-07 16:31:20 +0900

    Delete miniproject directory

--- miniproject/miniproject/src/main/java/com/example/miniproject/dto/CommentRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.miniproject.dto;
-
-import lombok.Getter;
-
-@Getter
-public class CommentRequestDto {
-    private String contents;
-}
--- miniproject/miniproject/src/main/java/com/example/miniproject/repository/BlackListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.miniproject.repository;
-
-import com.example.miniproject.entity.BlackList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlackListRepository extends JpaRepository<BlackList, Long> {
-    BlackList findByToken(String token);
-}
-
